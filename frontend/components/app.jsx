a11c0fd7f3183d4417096d8dcc5c11858cc63d55
Helen Cheung <helenkei.cheung@gmail.com>
2020-01-09 10:04:07 -0800

    refactored header into its own component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import {
     Route,
     Redirect,
@@ -9,26 +8,14 @@ import {
 } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
-import GreetingContainer from './greeting/greeting_container';
+import Header from './header/header';
+
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 
 const App = () => (
     <div>
-        <div className="legacy-top-bar clearfix" />
-
-        <header className='top-bar clearfix'>
-            <div className='top-bar-logo'>
-                <Link to="/">
-                    <img src="/assets/trashcan-logo" alt="dumpster logo" className='top-bar-logo-img'/>
-                </Link>
-                <Link to="/">
-                    <h1>OpenDumpster</h1>
-                </Link>
-            </div>
-
-            <GreetingContainer />
-        </header>
+        <Header />
 
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/signup" component={SignupFormContainer} />
---
+++ frontend/components/header/header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import GreetingContainer from '../greeting/greeting_container';
+
+const Header = () => (
+    <header>
+        <div className="legacy-top-bar clearfix" />
+
+        <div className='top-bar clearfix'>
+            <div className='top-bar-logo'>
+                <Link to="/">
+                    <img src="/assets/trashcan-logo" alt="dumpster logo" className='top-bar-logo-img' />
+                </Link>
+                <Link to="/">
+                    <h1>OpenDumpster</h1>
+                </Link>
+            </div>
+
+            <GreetingContainer />
+        </div>
+    </header>
+);
+
+export default Header;
