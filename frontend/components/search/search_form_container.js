7ceba5b77452df0a2ec960c4b9b05318f7e333fa
Helen Cheung <helenkei.cheung@gmail.com>
2020-02-13 01:19:34 -0800

    cleaned up unused imports

--- frontend/components/search/search_form_container.js
+++ frontend/components/search/search_form_container.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import { searchRestaurants } from '../../actions/restaurant_actions';
 import { receiveSearch } from '../../actions/search_actions';
